3aa6eb16be46aa571b0595b26845da0c7941b2aa
Daniel Alves <alvesdesouza.daniel@gmail.com>
2017-01-13 15:46:30 -0200

    atualizacao do script

---
+++ script.js
@@ -0,0 +1,30 @@
+var words = ["casa","arvore","problema","palavra","paralelepipedo","caderno","caneta","notebook","chinelo","jeronimo"];
+
+var sorteio = function() {
+    var machineOption = Math.floor((Math.random()*words.length));
+    console.log("Palavra sorteada: " + words[machineOption]);
+    return words[machineOption];
+}
+
+var palavraSorteada = sorteio();
+var letrasCertas = [];
+var letraErrada = [];
+
+
+function forca(letraUser) {
+
+    if (palavraSorteada.indexOf(letraUser)>= 0 ) {
+      for(var i=0 ; i < palavraSorteada.length ; i++){
+          var letra = palavraSorteada.charAt(i);
+          if (letraUser === letra) {
+              letrasCertas[i] = letraUser;
+          }
+      }
+    } else {
+        letraErrada.push(letraUser);
+    }
+
+  console.log("Entrada user: " + letraUser);
+  console.log("certas " + letrasCertas.toString());
+  console.log("Erradas " + letraErrada.toString());
+}
