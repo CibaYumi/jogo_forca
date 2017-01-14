6b8d5f3e1f52e281f750ab345ba7dd7f4eda1808
Rapha Prieto <fernandesprieto@gmail.com>
2017-01-14 12:53:29 -0200

    Atualização da Identação

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-var words = ["casa","arvore","problema","palavra","paralelepipedo","caderno","caneta","notebook","chinelo","jeronimo"];
+var words = ["casa", "arvore", "problema", "palavra", "paralelepipedo", "caderno", "caneta", "notebook", "chinelo", "jeronimo"];
 
 var sorteio = function() {
-    var machineOption = Math.floor((Math.random()*words.length));
+    var machineOption = Math.floor((Math.random() * words.length));
     console.log("Palavra sorteada: " + words[machineOption]);
     return words[machineOption];
 }
@@ -13,24 +13,24 @@ var letraErrada = [];
 
 function forca(letraUser) {
 
-  var letraUser = document.getElementById('validar').value;
+    var letraUser = document.getElementById('validar').value;
 
-  console.log("Jeronimo: " + letraUser);
-  if (palavraSorteada.indexOf(letraUser)>= 0 ) {
-    for(var i=0 ; i < palavraSorteada.length ; i++){
-        var letra = palavraSorteada.charAt(i);
-        if (letraUser === letra) {
-            letrasCertas[i] = letraUser;
+    console.log("Jeronimo: " + letraUser);
+    if (palavraSorteada.indexOf(letraUser) >= 0) {
+        for (var i = 0; i < palavraSorteada.length; i++) {
+            var letra = palavraSorteada.charAt(i);
+            if (letraUser === letra) {
+                letrasCertas[i] = letraUser;
+            }
         }
+    } else {
+        letraErrada.push(letraUser);
     }
-  } else {
-      letraErrada.push(letraUser);
-  }
 
-  document.getElementById("jeronimo").innerHTML = letrasCertas.toString();
+    document.getElementById("jeronimo").innerHTML = letrasCertas.toString();
 
 
-  console.log("Entrada user: " + letraUser);
-  console.log("certas " + letrasCertas.toString());
-  console.log("Erradas " + letraErrada.toString());
+    console.log("Entrada user: " + letraUser);
+    console.log("certas " + letrasCertas.toString());
+    console.log("Erradas " + letraErrada.toString());
 }
